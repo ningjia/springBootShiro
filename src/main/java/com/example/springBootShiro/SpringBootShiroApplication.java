6b5dc387e2583be27bed6ed72dabac8818c594bb
neil.jia <neil.jia@elitesland.com>
2018-09-03 12:50:22 +0800

    调整代码以便符合编码规范

--- src/main/java/com/example/springBootShiro/SpringBootShiroApplication.java
+++ src/main/java/com/example/springBootShiro/SpringBootShiroApplication.java
@@ -3,6 +3,12 @@ package com.example.springBootShiro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * describe:springboot的main()方法类
+ *
+ * @author xxx
+ * @date 2018/08/16
+ */
 @SpringBootApplication
 public class SpringBootShiroApplication {
 
--- src/main/java/com/example/springBootShiro/common/Constant.java
+++ src/main/java/com/example/springBootShiro/common/Constant.java
@@ -1,5 +1,11 @@
 package com.example.springBootShiro.common;
 
+/**
+ * describe:保存用户名密码/角色权限等常量的接口类
+ *
+ * @author xxx
+ * @date 2018/08/16
+ */
 public interface Constant {
     public static String LOGIN_NAME_ADMIN = "admin";
     public static String LOGIN_NAME_USER = "user";
